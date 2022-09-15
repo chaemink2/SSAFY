f7e4efe42fa728415231b495417e1d817170dd9c
채민기 <109517707+chaemink2@users.noreply.github.com>
2022-09-15 22:12:04 +0900

    Add files via upload

---
+++ 09.13 코드2/vue-cli-bootstrap/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <b-form-input 
+        v-model="myName"
+        placeholder="이름을 입력하세요">            
+        </b-form-input>
+        <div>이름: {{myName}}</div>
+        <div>
+            <b-spinner label="로딩중"></b-spinner>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            myName: "",
+        };
+    },
+};
+</script>
+
+<style>
+
+</style>
