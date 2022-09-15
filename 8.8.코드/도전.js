21fe4852bad36700b17a58e012efbbdff35434a7
채민기 <109517707+chaemink2@users.noreply.github.com>
2022-09-15 21:45:28 +0900

    Add files via upload
    web_front-back

---
+++ 8.8.코드/도전.js
@@ -0,0 +1,14 @@
+const a = "ABCABCMCABCABCMCBBQABC";
+
+const a1 = a.indexOf("ABC");
+const b = 0;
+
+const array = [];
+for (let i = 0; i < a.length; i++)
+{
+    if (console.log(a1))
+    {
+        b++;
+    }
+}
+console.log(b);
