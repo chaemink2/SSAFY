386f3eefa85c5c2841fd1776b4d105cbda680867
채민기 <109517707+chaemink2@users.noreply.github.com>
2022-09-15 21:58:49 +0900

    Delete 8.9 코드 directory

--- 8.9 코드/002 각요소의 값 찾기.js
+++
@@ -1,8 +0,0 @@
-const array = [4,3,5,1,6,5];
-let count = 0;
-
-array.forEach((element) =>{
-  if(element % 2 === 1) count++;
-})
-
-console.log(count)
--- 8.9 코드/비동기.js
+++
@@ -1,4 +0,0 @@
-const result = axios.get("https://jsonplaceholder.typicode.com/todos")
-const array = [];
-array.push(result.data);
-console.log(array)
