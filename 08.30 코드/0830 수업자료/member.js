e564d911219ad0a3816add5bfdacd8260acd50b5
채민기 <109517707+chaemink2@users.noreply.github.com>
2022-09-16 14:35:18 +0900

    Add files via upload

---
+++ 08.30 코드/0830 수업자료/member.js
@@ -0,0 +1,6 @@
+const number = {
+  number1 : 30,
+  number2 : 50,
+}
+
+module.exports = number;
---
+++ 08.30 코드/0830 수업자료/sum.js
@@ -0,0 +1,5 @@
+const{number1, number2} = require("./member");
+
+const answer = number1 + number2;
+
+module.exports = answer;
