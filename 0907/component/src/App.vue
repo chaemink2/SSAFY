6b8c0f360e82d2486b9c9527c7c6c66ae42f58f4
채민기 <109517707+chaemink2@users.noreply.github.com>
2022-09-16 14:47:33 +0900

    Add files via upload

---
+++ 0907/component/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    app
+    <Header></Header>
+    <Main></Main>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue"
+import Main from "./components/Main.vue"
+export default {
+  components: {
+    Header,
+    Main,
+  }
+}
+</script>
+
+<style>
+
+</style>
